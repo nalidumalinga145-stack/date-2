d0d118e16604edc8d62735af13439ec04465d527
nalidumalinga145-stack <nalidumalinga145@gmail.com>
2026-09-23 11:27:14 +0530

    name

--- Sample.java/Sample.java
+++ Sample.java/Sample.java
@@ -1,8 +1,7 @@
 class Sample{
     public static void main(String args []){
-        System.out.prtinln("hello");
-        System.out.prtinln("hello world")""
-
+        System.out.prtinln("nalidu malinga");
+       
     }
 
 }
